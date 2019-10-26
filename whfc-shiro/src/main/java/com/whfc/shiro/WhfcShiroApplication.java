a2865522e01decb4f0d6f72fcde34d666347e8be
likang <383158628@qq.com>
2019-10-26 16:11:30 +0800

    创建shiro权限管理项目

---
+++ whfc-shiro/src/main/java/com/whfc/shiro/WhfcShiroApplication.java
@@ -0,0 +1,13 @@
+package com.whfc.shiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhfcShiroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhfcShiroApplication.class, args);
+    }
+
+}
---
+++ whfc-shiro/src/test/java/com/whfc/shiro/WhfcShiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whfc.shiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhfcShiroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
